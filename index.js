26279039621b4ba2a992a4cc479b25792ca9178b
forhadbinamir <forhadbinamir@gmail.com>
2022-04-27 14:28:15 +0300

    emaJon server

---
+++ index.js
@@ -0,0 +1,64 @@
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+// middleware 
+
+app.use(cors())
+app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ekasn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect()
+        const productCollection = client.db('emajondata').collection('products')
+        app.get('/product', async (req, res) => {
+            console.log('query', req.query)
+            const page = parseInt(req.query.page)
+            const size = parseInt(req.query.size)
+
+            const query = {}
+            const cursor = productCollection.find(query)
+            let products;
+            if (page || size) {
+                products = await cursor.skip(page * size).limit(size).toArray()
+            }
+            else {
+                products = await cursor.toArray()
+            }
+            res.send(products)
+        })
+        app.get('/productCount', async (req, res) => {
+            const count = await productCollection.estimatedDocumentCount()
+            res.send({ count })
+        })
+
+        // post data by keys
+        app.post('/productKey', async (req, res) => {
+            const keys = req.body;
+            const ids = keys.map(id => ObjectId(id))
+            const query = { _id: { $in: ids } }
+            const cursor = productCollection.find(query)
+            const product = await cursor.toArray()
+            res.send(product)
+        })
+    }
+    finally { }
+}
+run().catch(console.dir)
+
+app.get('/', (req, res) => {
+    res.send('emaJon server is running')
+})
+
+app.listen(port, () => {
+    console.log('Port is running', port)
+})
